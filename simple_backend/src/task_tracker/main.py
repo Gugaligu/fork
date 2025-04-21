738718b9924d3402fd235ce1c8526c0f513d5c82
Artem Sharanov <sharanov-2014@mail.ru>
2025-04-21 21:49:49 +0500

    save in variable

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,37 @@
+from enum import Enum
+
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
+j={}
+class Status(Enum):
+    active = "active"
+    unactive = "unactive"
+    completed = "completed"
+
 @app.get("/tasks")
 def get_tasks():
-    pass
+    return j
 
 @app.post("/tasks")
 def create_task(task):
-    pass
+    j[len(j)+1]={"task":task,"status":Status.unactive}
+    return j
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+def update_task(task_id: int, new_task:str=None,status:Status=None):
+    if new_task is None:
+        j[task_id]["task"] = new_task
+    if status is None:
+        j[task_id]["status"]=status
+    return j
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    j.pop(task_id)
+    return j
+
+if __name__ == '__main__':
+    uvicorn.run("main:app",reload=True)

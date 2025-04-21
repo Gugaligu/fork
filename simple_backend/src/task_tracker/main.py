d5565eda51c1b526237dd783aaf7eb84badf6121
Artem Sharanov <sharanov-2014@mail.ru>
2025-04-21 20:56:48 +0500

    база данных в переменной

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,28 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
+j={}
+
 @app.get("/tasks")
 def get_tasks():
-    pass
+    return j
 
 @app.post("/tasks")
 def create_task(task):
-    pass
+    j[len(j)+1]=task
+    return j
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+def update_task(task_id: int, new_data):
+    j[task_id]=new_data
+    return j
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    j.pop(task_id)
+    return j
+
+if __name__ == '__main__':
+    uvicorn.run("main:app",reload=True)
